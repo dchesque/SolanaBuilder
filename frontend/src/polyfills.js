f620e6d3c3124188a37aa5ef510089e3ef182c85
dchesque <dchesque@gmail.com>
2025-02-25 00:05:21 -0300

    deploy vercel ajuste 6 - 3.7sonnet

--- frontend/src/polyfills.js
+++ frontend/src/polyfills.js
@@ -1,5 +1,20 @@
+// src/polyfills.js
 import { Buffer } from 'buffer';
-import process from 'process';
+import process from 'process/browser';
 
+// Fix for console-browserify
+import consoleBrowserify from 'console-browserify';
+
+// Make polyfills available globally
 window.Buffer = Buffer;
 window.process = process;
+
+// Only override console if it doesn't exist (which shouldn't happen in a browser)
+if (!window.console) {
+  window.console = consoleBrowserify;
+}
+
+// Add any additional Solana-specific polyfills if needed
+if (typeof window !== 'undefined') {
+  window.global = window;
+}
