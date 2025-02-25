f41ed2a33c0946cc9e26ddc36cd933b87b914935
dchesque <dchesque@gmail.com>
2025-02-24 23:06:45 -0300

    deploy frontend vercel 01

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -1,9 +1,15 @@
 const webpack = require('webpack');
 const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
+const path = require('path');
 
 module.exports = {
   webpack: {
     configure: (webpackConfig) => {
+      // Remove ModuleScopePlugin para permitir imports fora de src
+      webpackConfig.resolve.plugins = webpackConfig.resolve.plugins.filter(
+        (plugin) => plugin.constructor.name !== 'ModuleScopePlugin'
+      );
+
       // Substitui todos os imports de process/browser por process/browser.js
       webpackConfig.module.rules.push({
         test: /[\\/]node_modules[\\/].*axios[\\/].*\.js$/,
